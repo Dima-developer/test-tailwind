9481e44b57e80c843f8078d99ef71bb653d114cc
Dmytro Petrenko <petrenko.d28@gmail.com>
2021-07-05 16:34:11 +0300

    add test-tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: {
+    enebled: true,
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    options: {
+      keyframes: true,
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      courier: ["courier new"],
+    },
+    extend: {
+      animation: {
+        spin: "spin 3s linear infinite",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
